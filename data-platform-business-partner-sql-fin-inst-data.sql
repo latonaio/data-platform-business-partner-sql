07191529da6e65c632612fbb4bd660c23871a986
Yurino Oga <yurino.o@latona.io>
2022-11-20 21:55:04 +0900

    release

--- data-platform-business-partner-sql-fin-inst-data.sql
+++ data-platform-business-partner-sql-fin-inst-data.sql
@@ -1,25 +1,27 @@
-CREATE TABLE `data_platform_business_partner_fin_inst_data`  -- 新規追加
+CREATE TABLE `data_platform_business_partner_fin_inst_data`
 (
-  `BusinessPartner`           int(12) NOT NULL,              -- 新規追加
-  `FinInstIdentification`     int(4) NOT NULL,               -- 新規追加
-  `ValidityEndDate`           date NOT NULL,                 -- 新規追加
-  `ValidityStartDate`         date DEFAULT NULL,             -- 新規追加
-  `FinInstCountry`            varchar(3) DEFAULT NULL,       -- 新規追加
-  `FinInstNumber`             varchar(15) DEFAULT NULL,      -- 新規追加
-  `FinInstName`               varchar(100) DEFAULT NULL,     -- 新規追加
-  `FinInstBranchName`         varchar(100) DEFAULT NULL,     -- 新規追加
-  `SWIFTCode`                 varchar(11) DEFAULT NULL,      -- 新規追加
-  `InternalFinInstCustomerID` int(12) DEFAULT NULL,          -- 新規追加
-  `InternalFinInstAccountID`  int(12) DEFAULT NULL,          -- 新規追加
-  `FinInstControlKey`         varchar(2) DEFAULT NULL,       -- 新規追加
-  `FinInstAccountName`        varchar(40) DEFAULT NULL,      -- 新規追加
-  `FinInstAccount`            varchar(18) DEFAULT NULL,      -- 新規追加
-  `IsMarkedForDeletion`       tinyint(1) DEFAULT NULL,       -- 新規追加
+  `BusinessPartner`           int(12) NOT NULL,
+  `FinInstIdentification`     int(4) NOT NULL,
+  `ValidityEndDate`           date NOT NULL,
+  `ValidityStartDate`         date NOT NULL,
+  `FinInstCountry`            varchar(3) NOT NULL,
+  `FinInstCode`               varchar(10) NOT NULL,
+  `FinInstBranchCode`         varchar(10) NOT NULL,
+  `FinInstFullCode`           varchar(15) NOT NULL,
+  `FinInstName`               varchar(100) DEFAULT NULL,
+  `FinInstBranchName`         varchar(100) DEFAULT NULL,
+  `SWIFTCode`                 varchar(11) DEFAULT NULL,
+  `InternalFinInstCustomerID` int(12) DEFAULT NULL,
+  `InternalFinInstAccountID`  int(12) DEFAULT NULL,
+  `FinInstControlKey`         varchar(2) DEFAULT NULL,
+  `FinInstAccountName`        varchar(40) DEFAULT NULL,
+  `FinInstAccount`            varchar(18) DEFAULT NULL,
+  `IsMarkedForDeletion`       tinyint(1) DEFAULT NULL,
 
-  PRIMARY KEY (`BusinessPartner`, `FinInstIdentification`, `ValidityEndDate`),
+  PRIMARY KEY (`BusinessPartner`, `FinInstIdentification`, `ValidityEndDate`, `ValidityStartDate`),
 
   CONSTRAINT `DataPlatformBusinessPartnerFinInstData_fk` FOREIGN KEY (`BusinessPartner`) REFERENCES `data_platform_business_partner_general_data` (`BusinessPartner`),
-  CONSTRAINT `DataPlatformBusinessPartnerFinInstDataFinInstAccount_fk` FOREIGN KEY (`FinInstCountry`, `FinInstNumber`, `InternalFinInstCustomerID`, `InternalFinInstAccountID`, `FinInstControlKey`, `FinInstAccountName`, `FinInstAccount`) REFERENCES `data_platform_fin_inst_account_item_data` (`FinInstCountry`, `FinInstNumber`, `InternalFinInstCustomerID`, `InternalFinInstAccountID`, `FinInstControlKey`, `FinInstAccountName`, `FinInstAccount`)
+  CONSTRAINT `DataPlatformBusinessPartnerFinInstDataFinInstAccount_fk` FOREIGN KEY (`FinInstCountry`, `FinInstCode`, `FinInstBranchCode`, `FinInstFullCode`, `InternalFinInstCustomerID`, `InternalFinInstAccountID`) REFERENCES `data_platform_fin_inst_account_item_data` (`FinInstCountry`, `FinInstNumber`, `InternalFinInstCustomerID`, `InternalFinInstAccountID`)
 
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4;
